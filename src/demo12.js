a76f00856eb6d1ad94c71ec3ab1c421ca59b2bf2
hardikvasar1 <hardik.vasar@commercepundit.com>
2021-12-21 15:21:58 +0530

    1 file modified

--- src/demo12.js
+++ src/demo12.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './style.css';
-import Alert from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.css';
-export default function demo12() {
+export default function demo12(props) {
+  const Alert = () => {
+    return (
+      <>
+        <Alert>
+          <Alert.Heading>
+            This is a success alert which has green background
+          </Alert.Heading>
+        </Alert>
+      </>
+    );
+  };
   return (
     <>
-      <Alert text="OMG! Something really bad has happended!" />
+      <Alert />
     </>
   );
 }
