e3a717bfef50fa31302fda12910f4f8afd8b7462
hardikvasar1 <hardik.vasar@commercepundit.com>
2021-12-21 15:17:54 +0530

    1 file modified

--- src/demo12.js
+++ src/demo12.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './style.css';
-import PropTypes from 'prop-types';
+import Alert from 'react-bootstrap/Alert';
 import 'bootstrap/dist/css/bootstrap.css';
 export default function demo12() {
-  return <>Hello</>;
+  return (
+    <>
+      <Alert text="OMG! Something really bad has happended!" />
+    </>
+  );
 }
